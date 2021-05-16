94ed45bfdb06468423945ae0fb453b18794f8e3e
SanghwaLee <devlsh0124@gmail.com>
2021-05-16 15:17:32 +0900

    feat: Add Block Component

--- src/Components/Block.js
+++ src/Components/Block.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Block(props) {
-  return <div></div>;
+  let state;
+  switch (props.value) {
+    case -1:
+      state = "";
+      break;
+    case 0:
+      state = <span className="dot--white"></span>;
+      break;
+    case 1:
+      state = <span className="dot--black"></span>;
+      break;
+    default:
+      break;
+  }
+
+  return <div onClick={props.Onclick}>{state}</div>;
 }
 
 export default Block;
