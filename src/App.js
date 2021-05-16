2064578df71ee2f385ca8012670062d9d65f2c04
SanghwaLee <devlsh0124@gmail.com>
2021-05-16 11:19:23 +0900

    feat: Add App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./calculate.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Container"></div>
+    </div>
+  );
+}
+
+export default App;
