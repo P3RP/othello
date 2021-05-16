596444c3eaab021ab646b858250fc2fa478796e4
SanghwaLee <devlsh0124@gmail.com>
2021-05-16 14:16:14 +0900

    feat: Add Board

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,7 +2,32 @@ import React from "react";
 import Block from "./Block";
 
 function Board(props) {
-  return <Block></Block>;
+  const HEIGHT = 8;
+  const WIDTH = 8;
+
+  const trIdx = [];
+  for (let i = 0; i < HEIGHT; i++) {
+    trIdx[i] = i;
+  }
+
+  const tdIdx = [];
+  for (let i = 0; i < WIDTH; i++) {
+    tdIdx[i] = i;
+  }
+
+  const trs = trIdx.map((low) => (
+    <tr key={low}>
+      {tdIdx.map((col) => (
+        <Block className="Block" key={col} low={low} col={col} />
+      ))}
+    </tr>
+  ));
+
+  return (
+    <table>
+      <tbody>{trs}</tbody>
+    </table>
+  );
 }
 
 export default Board;
